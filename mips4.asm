f8ecdfb3f18d555bb719e60aed4edec691676db2
Erick Salas-Chaverri <ericksc@gmail.com>
2020-05-03 00:33:00 -0600

    Initial commit

---
+++ mips4.asm
@@ -0,0 +1,64 @@
+fir:
+	  .data
+in:	  .space  48		# suponer arreglo de 10 + 2 condiciones iniciales = 12. 12 * 4 = 48
+out:	  .space  40		# suponer arreglo de 10  condiciones iniciales = 10. 10 * 4 = 40
+firtaps:  .space  12		# suponer arreglo de 3  firtaps 3 * 4 = 12
+
+	.text
+__start:
+				# inicializando arreglo in
+	la	$t0, in		# "r3" Dirección base de in
+	li	$s2, 1
+	#sw $s2, 0($t0)
+	sw $s2, 4($t0)
+	sw $s2, 8($t0)		#  inicializando el 3er elemento del arreglo in como 1 para tener vector inicial [0,0,1,0,0,0,0,0...]
+	sw $s2, 12($t0)
+	
+				# inicializando arreglo out
+	la	$t2, out	# "r4" Dirección base de out
+
+				# inicializando arreglo firtaps
+	la	$t1, firtaps	# "r5" Dirección base de firtaps
+	li $s2, 1
+	sw $s2, ($t1)
+	li $s2, -2
+	sw $s2, 4($t1)
+	li $s2, -3
+	sw $s2, 8($t1)
+
+				# inicializando numtaps
+	li $s0, 3	        # "r1" numtaps
+
+	li $s1, 0	        # "r2" sampleidx
+					
+	li $t4, 0            # $t3 sea outval = 0. inicialmente
+	li $t3, 0            # $t4 sea tapidx = 0. inicialmente
+	li $t5, 6            # $t5 sea size = 6.	
+	
+$L4:
+        # tapidx<numtaps
+        slt     $s2,$t3,$s0  
+        beq     $s2,$0,$L3
+        
+        # sampleidx + tapidx      
+        addu $s2, $s1 , $t3
+        sll     $s2,$s2,2
+
+        addu    $s2,$t0,$s2
+        lw      $s3,0($s2)
+            
+        sll     $s2,$t3,2
+
+        addu    $s4,$t1,$s2
+        lw      $s2,0($s4)
+        
+        mul  $s2,  $s2, $s3
+        add $t4, $t4, $s2
+
+	addiu $t3, $t3 ,1
+        b       $L4
+
+$L3:
+        sll     $s2,$s1,2
+        addu    $s2,$t2,$s2
+        sw      $t4,0($s2)
