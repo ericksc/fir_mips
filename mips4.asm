9b0be9f231fb4e78674e8a5db4b7dea0edd2f1f8
Erick Salas-Chaverri <ericksc@gmail.com>
2020-05-03 00:58:30 -0600

    Update mips4.asm

--- mips4.asm
+++ mips4.asm
@@ -37,34 +37,34 @@ __start:
 	
 $L4:
         # tapidx<numtaps
-        slt     $s2,$t3,$s0  
-        beq     $s2,$0,$L3
+        slt     $s2,$t3,$s0  	# $s2 <- cmpgt($t2,$s0)
+        beq     $s2,$0,$L3	# bcond($s2, $L3)
         
         # sampleidx + tapidx      
-        addu $s2, $s1 , $t3
+        addu $s2, $s1 , $t3	# $s1 <- add($s1 , $t3)
         
-        li $s4, 4
-        mul $s2, $s2, $s4
+        li $s4, 4		# $s4 <- movi(4)
+        mul $s2, $s2, $s4	# $s2 <- mul($s2, $s4)
 
-        addu    $s2,$t0,$s2
-        lw      $s3,0($s2)
+        addu    $s2,$t0,$s2     # $s2 <- add($t0 , $s2)
+        lw      $s3,0($s2)	# $s3 <- idi($s2)
         
-        li $s4, 4
-        mul $s2, $t3, $s4
+        li $s4, 4		# $s4 <- movi(4)
+        mul $s2, $t3, $s4	# $s2 <- mul($t3, $s4)
 
-        addu    $s4,$t1,$s2
-        lw      $s2,0($s4)
+        addu    $s4,$t1,$s2	# $s2 <- add($t0 , $s2)
+        lw      $s2,0($s4)	# $s2 <- idi($s4)
         
-        mul  $s2,  $s2, $s3
-        add $t4, $t4, $s2
+        mul  $s2,  $s2, $s3	# $s2 <- mul($s2, $s3)
+        add $t4, $t4, $s2	# $t4 <- add($t4 , $s2)
 
-	addiu $t3, $t3 ,1
-        b       $L4
+	li $s4, 1		# $s4 <- movi(1)
+	add $t3, $t3 ,$s4	# $t3 <- add($t3 , $s4)
+        b       $L4		# branch ($L4)
 
 $L3:
+        li $s4, 4		# $s4 <- movi(4)
+        mul $s2, $s1, $s4	# $s2 <- mul($s1, $s4)
 
-        li $s4, 4
-        mul $s2, $s1, $s4
-
-        addu    $s2,$t2,$s2
-        sw      $t4,0($s2)
+        addu    $s2,$t2,$s2	# $s2 <- add($t2 , $s2)
+        sw      $t4,0($s2)	# sti($s2, $t4)
